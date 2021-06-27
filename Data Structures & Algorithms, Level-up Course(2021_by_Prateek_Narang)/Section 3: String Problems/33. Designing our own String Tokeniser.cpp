c7a1380d637b015bc43d6b6cad0ee19e3224b788
Jitendra Kumar <jitendrar048@gmail.com>
2021-06-27 22:29:06 +0530

    33. Designing our own String Tokeniser!

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 3: String Problems/33. Designing our own String Tokeniser.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char *mystrtok(char *str, char delim)
+{
+    // static
+    static char *input = NULL;
+    if (str != NULL)
+    {
+        input = str;
+    }
+    if (input == NULL)
+        return NULL;
+
+    // Why dynamic arrays? We can return dynamic arrays not static arrays.
+    char *token = new char[strlen(input) + 1]; // +1 for the null character
+    int i = 0;
+    for (; input[i] != '\0'; i++)
+    {
+        if (input[i] != delim)
+        {
+            token[i] = input[i];
+        }
+        else
+        {
+            token[i] = '\0';
+            input = input + i + 1;
+            return token;
+        }
+    }
+
+    // out of the loop
+    token[i] = '\0';
+
+    // reset the input as NULL
+    input = NULL;
+
+    return token;
+}
+
+int main()
+{
+    char input[1000];
+    cin.getline(input, 1000);
+
+    char *token = mystrtok(input, ' ');
+
+    while (token != NULL)
+    {
+        cout << token << endl;
+        token = mystrtok(NULL, ' ');
+    }
+}
+/* Why dynamic array? 
+Because we want to return the array 
+If we create an static array. Then it will be local variable 
+and it will get destroyed when the function call ends.
+*/
