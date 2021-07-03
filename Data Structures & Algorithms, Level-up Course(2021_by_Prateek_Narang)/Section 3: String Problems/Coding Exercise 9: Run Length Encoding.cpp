f1737bd930cdd079bf0072b556658958c46805de
Jitendra Kumar <jitendrar048@gmail.com>
2021-07-03 12:25:19 +0530

    Coding Exercise 9: Run Length Encoding

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 3: String Problems/Coding Exercise 9: Run Length Encoding.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/string-compression/
+
+// Time = O(N), Space = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//str is the input the string
+string compressString(const string &str)
+{
+    string res = "";
+
+    int cnt = 0;
+    char ch = str[0];
+    for (int i = 0; i < str.length(); i++)
+    {
+        // count the freq of char
+        while (ch == str[i] && i < str.length())
+        {
+            cnt++;
+            i++;
+        }
+
+        // Append the char+ freqofChar into res
+        res += (ch + to_string(cnt));
+
+        // Reset the values
+        ch = str[i];
+        cnt = 0;
+        i--;
+    }
+
+    return res.length() < str.length() ? res : str;
+}
