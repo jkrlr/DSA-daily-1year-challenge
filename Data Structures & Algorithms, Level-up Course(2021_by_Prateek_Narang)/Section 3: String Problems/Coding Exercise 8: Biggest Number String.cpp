d44c9dddf70951991c7032244415f854deca0cb0
Jitendra Kumar <jitendrar048@gmail.com>
2021-07-04 20:52:05 +0530

    Correct "comp" code added

--- Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 3: String Problems/Coding Exercise 8: Biggest Number String.cpp
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 3: String Problems/Coding Exercise 8: Biggest Number String.cpp
@@ -1,43 +1,35 @@
-// Input  : 10,11,20,303,3
+// https://leetcode.com/problems/largest-number/solution/
 
-// After Sorting : 3, 303, 30, 11, 10
-// Output : 3303201110 => largestNum
+/* Input  : 10,11,20,303,3
+After Sorting : 3, 303, 30, 11, 10
+Output : 3303201110 => largestNum */
 
-// Time = O(Sort + N*N) = O(NlogN * log(Max_Size_of_Int)) = O(NlogN*9) = O(NlogN + N*N) = O(N^2) , Space = O(1) // N is length of array
+// Time = O(Sort) = O(NlogN * log(Max_Size_of_Int)) = O(NlogN*9) = O(NlogN) , Space = O(1) // N is length of array
 
 #include <bits/stdc++.h>
 using namespace std;
 
 bool comp(int a, int b)
 {
+    string comb1 = to_string(a) + to_string(b);
+    string comb2 = to_string(b) + to_string(a);
 
-    int d1 = pow(10, floor(log10(a))); // pow(base, exponent) => <cmath>
-    int d2 = pow(10, floor(log10(b))); // floor(log10(num)) => gives no. of digits in num
-
-    // Comparing the leftmost digits
-    while (d1 > 0 and d2 > 0)
-    {
-        if ((a / d1) > (b / d2))
-            return true;
-        else if ((a / d1) < (b / d2))
-            return false;
-        d1 /= 10;
-        d2 /= 10;
-    }
-
-    return a < b; // This is 3 < 303 case
+    return comb1 > comb2;
 }
 
 string concatenate(vector<int> numbers)
 {
-    //complete this method and return the largest number you can form as a string
-
     // Sort
     sort(numbers.begin(), numbers.end(), comp);
 
+    // If, after being sorted, the largest number is 0,
+    // the entire number is zero.
+    if (numbers[0] == 0)
+        return "0";
+
     string largestNum = "";
     for (int num : numbers)
-        largestNum += to_string(num); //Typically, slicing and concatenating strings require O(n) time
+        largestNum += to_string(num); //Considering it O(1) But Typically, slicing and concatenating strings require O(n) time
 
     return largestNum;
 }
