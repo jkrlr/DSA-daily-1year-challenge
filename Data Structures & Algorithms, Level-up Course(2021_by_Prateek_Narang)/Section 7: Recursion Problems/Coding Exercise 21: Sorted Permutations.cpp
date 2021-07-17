3d88cf86cf2dea39f4bbe86813d84a0d4bc9739d
Jitendra Kumar <jitendrar048@gmail.com>
2021-07-17 12:50:42 +0530

    Coding Exercise 21: Sorted Permutations[Backtracking Question]

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 7: Recursion Problems/Coding Exercise 21: Sorted Permutations.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+set<string> st;
+
+void SortedPermutationsHelper(string s, string output, int start)
+{
+    // Base Case
+    if (start == s.length()) // s[start]=='\0'
+    {
+        st.insert(output);
+        return;
+    }
+
+    // Recursive Case
+    for (int idx = start; idx < s.size(); idx++)
+    {
+        swap(s[start], s[idx]);
+        SortedPermutationsHelper(s, output + s[start], start + 1);
+        swap(s[start], s[idx]);
+    }
+}
+
+vector<string> findSortedPermutations(string s)
+{
+    //Your code goes here. Do not change the function or parameters. You can use helper functions if needed.
+    st.clear();
+    string output;
+    SortedPermutationsHelper(s, output, 0);
+
+    vector<string> res;
+
+    for (auto &str : st)
+    {
+        res.push_back(str);
+    }
+
+    return res;
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+
+    vector<string> res = findSortedPermutations(input);
+    for (auto str : res)
+        cout << str << " ";
+}
