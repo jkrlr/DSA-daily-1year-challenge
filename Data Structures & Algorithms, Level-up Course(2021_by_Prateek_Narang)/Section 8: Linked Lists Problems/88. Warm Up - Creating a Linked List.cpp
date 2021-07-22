b9cac39a24653c3e24b3b820edb5fe6dd1c6cdb2
Jitendra Kumar <jitendrar048@gmail.com>
2021-07-22 15:18:11 +0530

    88. Warm Up - Creating a Linked List

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 8: Linked Lists Problems/88. Warm Up - Creating a Linked List.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+//-----------Linked List--------//
+void insertAtHead(node *&head, int data)
+{
+    if (head == NULL)
+    {
+        head = new node(data);
+        return;
+    }
+
+    // otherwise
+    node *n = new node(data);
+    n->next = head;
+    head = n;
+}
+
+void printLL(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << "-->";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtHead(head, 4);
+    insertAtHead(head, 3);
+    insertAtHead(head, 2);
+    insertAtHead(head, 1);
+
+    printLL(head);
+}
