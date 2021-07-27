7097612a97dccfdc315c514d3b4c835962808844
Jitendra Kumar <jitendrar048@gmail.com>
2021-07-27 19:29:33 +0530

    Coding Exercise 33: First Non-Repeating Character

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 9: Stacks & Queue Problems/Coding Exercise 33: First Non-Repeating Character.cpp
@@ -0,0 +1,30 @@
+// Time =O(n), Space = O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<char> FindFirstNonRepeatingCharacter(string S)
+{
+    unordered_map<char, int> freq;
+    queue<char> q;
+    vector<char> res;
+
+    for (char ch : S)
+    {
+        freq[ch]++;
+
+        if (freq[ch] == 1)
+            q.push(ch);
+
+        // remove all the chars from front of queue till you get char with freq 1
+        while (!q.empty() && freq[q.front()] >= 2)
+            q.pop();
+
+        if (!q.empty())
+            res.push_back(q.front());
+        else
+            res.push_back('0');
+    }
+
+    return res;
+}
