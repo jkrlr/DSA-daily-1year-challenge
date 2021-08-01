9af4d557ffb8faec05a199a9cc0a9e054954d6b0
Jitendra Kumar <jitendrar048@gmail.com>
2021-08-01 10:43:07 +0530

    113. Build Tree - Boilerplate Code

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 10: Binary Trees Problems/113. Build Tree - Boilerplate Code.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class node{
+
+public:
+    int data;
+    node *left;
+    node *right;
+    
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Input : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+// preOrderBuild => Root-Left-Right
+node *buildTree(){
+    int d;
+    cin >> d;
+
+    if(d==-1){
+        return NULL;
+    }
+
+    node *n = new node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+
+    return n;
+}
+
+int main(){
+    node *root = buildTree();
+    //levelOrderPrint(root);
+}
