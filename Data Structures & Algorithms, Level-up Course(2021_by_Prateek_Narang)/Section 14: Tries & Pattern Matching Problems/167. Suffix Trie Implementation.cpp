e44e95cc72b3b7c56863d90962001c90b292c785
Jitendra Kumar <jitendrar048@gmail.com>
2021-08-15 23:37:33 +0530

    167. Suffix Trie Implementation

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 14: Tries & Pattern Matching Problems/167. Suffix Trie Implementation.cpp
@@ -0,0 +1,77 @@
+// Time - Insert = O(N^2*W) , Search = O(W), Space = (nC2 = N^2)
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+ 
+class Node{
+public:
+    char data;
+    unordered_map<char,Node*>m; // hash map by default is empty
+    bool isTerminal;
+ 
+    Node(char d){
+        data = d;
+        isTerminal = false;
+    }
+};
+ 
+class SuffixTrie{
+public:
+    Node *root;
+ 
+    SuffixTrie(){
+        root = new Node('\0');
+    }
+ 
+    void insert_helper(string word){
+        Node *temp = root;
+        
+        for(char ch: word){
+            if(temp->m.count(ch)==0){
+                Node *newNode = new Node(ch);
+                temp->m[ch] = newNode;
+            }
+            temp = temp->m[ch];
+        }
+        temp->isTerminal = true;
+    }
+    
+    void insert(string word){
+        // pick all the substrings from (i,eos) and call insert_helper
+        for(int i=0; word[i]!='\0'; i++){
+            insert_helper(word.substr(i));
+        }
+    }
+ 
+    bool search(string word){
+        Node *temp = root;
+        
+        for(char ch : word){
+            if(temp->m.count(ch)==0){
+                return false;
+            }
+            temp = temp->m[ch];
+        }
+        
+        return temp->isTerminal;
+    }
+};
+ 
+int main(){
+    string input = "hello";
+    string suffixes[] = {"lo", "ell", "hello"};
+    
+    SuffixTrie t;
+    t.insert(input);
+    
+    for(auto suffix : suffixes){
+        if(t.search(suffix)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
