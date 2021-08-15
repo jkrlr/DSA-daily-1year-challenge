f300e752a5fc536b10a420695653bb5679620b8f
Jitendra Kumar <jitendrar048@gmail.com>
2021-08-15 22:57:30 +0530

    165. Prefix Trie Implementation

---
+++ Data Structures & Algorithms, Level-up Course(2021_by_Prateek_Narang)/Section 14: Tries & Pattern Matching Problems/165. Prefix Trie Implementation.cpp
@@ -0,0 +1,76 @@
+// Time : Insert,Search - O(KeyLength), Space = O(N)
+// Delete funtion of trie - implement it by yourself
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+ 
+class Node{
+public:
+	char data;
+	unordered_map<char,Node*>m; 
+	bool isTerminal;
+ 
+	Node(char d){
+        // hash map by default is empty - no need to initialize it
+		data = d;
+		isTerminal = false;
+	}
+};
+ 
+class Trie{
+public:
+	Node *root;
+ 
+	Trie(){
+		root = new Node('\0');
+	}
+ 
+	void insert(string word){
+		Node *temp = root;
+		
+		for(char ch: word){
+			if(temp->m.count(ch)==0){
+				Node *newNode = new Node(ch);
+				temp->m[ch] = newNode;
+			}
+			temp = temp->m[ch];
+		}
+		temp->isTerminal = true;
+	}
+ 
+	bool search(string word){
+		Node *temp = root;
+		
+		for(char ch : word){
+			if(temp->m.count(ch)==0){
+				return false;
+			}
+			temp = temp->m[ch];
+		}
+		
+		return temp->isTerminal;
+	}
+};
+ 
+int main(){
+	string words[] = {"apple", "ape", "no", "news", "not", "never"};
+	Trie t;
+	for(auto word : words){
+		t.insert(word);
+	}
+	
+	int q;
+	cin>>q;
+ 
+	string search_word;
+	while(q--){
+		cin>>search_word;
+		if(t.search(search_word)){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+	}
+}
