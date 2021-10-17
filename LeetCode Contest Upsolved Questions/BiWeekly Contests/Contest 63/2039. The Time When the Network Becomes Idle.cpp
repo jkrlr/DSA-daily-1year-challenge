f63c1f0e05459b8142a88acf7702bacf7477267e
Jitendra Kumar <jitendrar048@gmail.com>
2021-10-17 19:10:11 +0530

    BiWeekly Contest 63[Upsolving]
    *2039. The Time When the Network Becomes Idle
    *2040. Kth Smallest Product of Two Sorted Arrays
    *Revision Problems

---
+++ LeetCode Contest Upsolved Questions/BiWeekly Contests/Contest 63/2039. The Time When the Network Becomes Idle.cpp
@@ -0,0 +1,76 @@
+// https://leetcode.com/problems/the-time-when-the-network-becomes-idle/
+
+// Medium
+
+// Tags - BFS-SSSP + Math
+
+// What this problem wants to teach me?
+// BFS variations with some tricky math logic
+
+// Time - O(V + E), Space = O(V+E)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> bfs(vector<vector<int>> &adj){
+        int n = adj.size();
+
+        vector<int> time(n, -1);    // distance array
+        queue<int> q;
+        int src = 0;
+        q.push(src); // Insert the src node into queue
+
+        time[src] = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(int nbr : adj[node]){
+                if(time[nbr]==-1){  // here distance array can be used as visited array also
+                    time[nbr] = time[node] + 1;
+                    q.push(nbr);
+                }
+            }
+        }
+
+        return time;
+    }
+
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        int n = edges.size();   // nodes in graph - all nodes are connected here
+
+        // create the graph
+        vector<vector<int>> adj(n);
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> time = bfs(adj);    // return the shortest time to reach the src i.e. node 0  from the particular node
+
+        int networkIdleTime = 0;
+
+        for(int i = 1; i < patience.size(); i++){
+            int sentMsgCount = (2 * time[i] - 1) / patience[i];
+
+            int lastMsgFromithNodeOutTime = sentMsgCount * patience[i];
+            int lastMsgFromithNodeInTime = lastMsgFromithNodeOutTime + 2 * time[i];   // 2*time[i] => going + returning time
+
+            networkIdleTime = max(networkIdleTime, lastMsgFromithNodeInTime);
+        }
+
+
+		// since the problem is asking for min time when there is no message is passing.
+		// and at "networkIdleTime" time the last message has arrived at one of the data servers.
+		// so at networkIdleTime + 1 no message will be passing between servers.
+
+        return networkIdleTime + 1;
+    }
+};
---
+++ LeetCode Contest Upsolved Questions/BiWeekly Contests/Contest 63/2040. Kth Smallest Product of Two Sorted Arrays.cpp
@@ -0,0 +1,119 @@
+// https://leetcode.com/problems/kth-smallest-product-of-two-sorted-arrays/
+
+// Hard
+
+// Revision
+
+// Tags - Binary Search-Search Space 
+
+// What this problem wants to teach me?
+// If arrays given are sorted and we can't use binary search directly
+// Then, we should think in terms of search space for our ans by finding ans for a particular value for given inputs
+
+/* Intuition
+    Binary search the result, and count the number of smaller product.
+
+    We can firstly consider the simple situation with no negatives.
+    The base of this question is the two sum problem.
+
+    In two sum problem,
+    we can count the number of pair with sum <= x.
+
+    In this two product problem,
+    we need to count the number of pair with product <= x. */
+
+/* Let check(x) be the answer to the question: how many products are less or equal than x.
+Then we use binary search and find the first moment where we have exactly k such numbers . */
+
+// Time = O(M*(LogN)*Log(10^10)), Space = O(M+N)   M = nums1.size(), N = nums2.size()
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+public:
+    int findMaxIndex(vector<int>& nums2, long long num, long long midVal){
+        int leftIndex = 0;
+        int rightIndex = nums2.size() - 1;
+        int resultIndex = -1;
+
+        while(leftIndex <= rightIndex){
+
+            int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if(num * nums2[midIndex] <= midVal){
+                resultIndex = midIndex;
+                leftIndex = midIndex + 1;
+            }
+            else{
+                rightIndex = midIndex - 1;
+            }
+        }
+    }
+
+    int findMinIndex(vector<int>& nums2, long long num, long long midVal){
+        int leftIndex = 0;
+        int rightIndex = nums2.size() - 1;
+        int resultIndex = nums2.size();
+
+        while(leftIndex <= rightIndex){
+
+            int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if(num * nums2[midIndex] <= midVal){
+                resultIndex = midIndex;
+                rightIndex = midIndex - 1;
+            }
+            else{
+                leftIndex = midIndex + 1;
+            }
+        }
+
+        return nums2.size() - resultIndex;
+    }
+
+    bool check(long long midVal, vector<int>& nums1, vector<int>& nums2, long long k){
+        long long cnt = 0;
+
+        for (int i = 0; i < nums1.size(); i++){
+            long long num = nums1[i];
+
+			//If num == 0, product of val and each element in nums2 will be 0. And if midVal>=0, then because all
+			//products are 0, all products will be smaller or equal to midVal. So we can add all products in the answer
+
+            if(num == 0 && midVal>=0){
+                cnt += nums2.size();
+            }
+            else if(num > 0){
+                cnt += findMaxIndex(nums2, num, midVal);
+            }
+            else if(num < 0){
+                cnt += findMinIndex(nums2, num, midVal);
+            }
+        }
+
+        return cnt >= k;
+    }
+
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        long long lo = (long long)-1e10;
+        long long hi = (long long)1e10;
+        int result = -1;
+
+        while(lo <= hi){
+
+            long long mid = lo + (hi - lo) / 2;
+
+            if(check(mid,nums1,nums2,k)){
+                result = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};
