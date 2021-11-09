75807dc9e442aa8c7a8102e24934c0c3123a7d83
Jitendra Kumar <jitendrar048@gmail.com>
2021-11-09 17:03:05 +0530

    1178. Number of Valid Words for Each Puzzle

---
+++ LeetCode Daily/1178. Number of Valid Words for Each Puzzle.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/
+
+// Hard
+
+// Tags - Array, HashTable, BitManipulation, Trie
+
+// What this problem wants to teach me?
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Complete all the approaches after end-Sem
+// Solution - Time = O(), Space = O()
+// https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/solution/
+// https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/discuss/1567324/C%2B%2BPython-Clean-Solutions-w-Detailed-Explanation-or-Bit-masking-and-Trie-Approaches
+// https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/discuss/371876/Detailed-Explanation-using-Trie-O(word_length-%2B-100*puzzle_length)
+// https://leetcode.com/problems/number-of-valid-words-for-each-puzzle/discuss/372025/3-cpp-solutions-bit-manipulation-trie-and-two-fold-hash.
