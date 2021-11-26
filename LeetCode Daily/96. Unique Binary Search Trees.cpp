58e1fbc3ddfa54c4ac37f5fbdb93eabec7a68937
Jitendra Kumar <jitendrar048@gmail.com>
2021-11-26 13:05:16 +0530

    Unique BST's [Catalan Number]

--- LeetCode Daily/96. Unique Binary Search Trees.cpp
+++ LeetCode Daily/96. Unique Binary Search Trees.cpp
@@ -5,6 +5,27 @@
 // Tags - Dp, Recursion, BST, Math-Catalan Number
 
 // What this problem wants to teach me?
+// Catalan Number Series : 1, 1, 2, 5, 14, 42, 132,...
+// Cn = CoCn-1 + C1Cn-2 + ... + Cn-1Co
+
+// Catalan Number F(n) = [Σ(i=1 to N)]F(i-1)*1*F(N-i) = (Left&Root&Right) = (2nCn)/(n+1)
+// FollowUps => Count(Binary Trees) = n! * F(n) -> we can inchange all the nodes in particular tree
+// FollowUps => Count(Unlabelled Binary Trees) = means with unique structure = Count(BST's) = F(n)
+
+/* Catalan Number Problems
+    1. No. Of BST's     => Cn
+    2. Ublabelled Trees => Cn
+    3. N parantheses (with and without letters) => Cn
+    4. Dyck Words       => Cn
+    5. Mountain Ranges  => Cn
+    6. Path on Grid     => Cn
+    7. Convex Polygon   => C(n-2)
+    8. Conect Disjoint Chords   => Cn
+ */
+
+// Tautorials
+// https://www.youtube.com/watch?v=0pTN0qzpt-Y&t=44s
+// https://www.youtube.com/watch?v=0s20L4-chDA&t=886s
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -37,15 +58,19 @@ public:
     }
 };  
 
-// Complete all below after endsem
+// DP-BottomUp | Time - O(N^2), Space - O(N)
+class Solution {
+public:
+    int numTrees(int N) {
+        vector<int>dp(N+1,0);
+        dp[0] = dp[1] = 1;
 
-// https://www.youtube.com/watch?v=0pTN0qzpt-Y&t=44s
-// https://www.youtube.com/watch?v=0s20L4-chDA&t=886s
+        for (int n = 2; n <= N; n++){
+            for (int i = 1; i <= n; i++){
+                dp[n] += dp[i - 1] * dp[n - i];
+            }
+        }
 
-// https://leetcode.com/problems/unique-binary-search-trees/discuss/1565543/C%2B%2BPython-5-Easy-Solutions-w-Explanation-or-Optimization-from-Brute-Force-to-DP-to-Catalan-O(N)
-// https://leetcode.com/problems/unique-binary-search-trees/discuss/1565599/C%2B%2B-EASY-INTUITIVE-SOL-(2-approaches)-oror-Recursive-DP-oror-Catalan-Numbers
-// https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
-
-// https://leetcode.com/problems/unique-binary-search-trees-ii/         Complete this question also
-
-
+        return dp[N];
+    }
+};  
